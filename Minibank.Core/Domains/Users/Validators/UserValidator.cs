7d9be1207e84765e06e0fed560b73270905ff160
“stacenko63” <amr_stacenko@mail.ru>
2022-04-07 18:21:11 +0300

    feat: do homework_5

--- Minibank.Core/Domains/Users/Validators/UserValidator.cs
+++ Minibank.Core/Domains/Users/Validators/UserValidator.cs
@@ -15,7 +15,7 @@ namespace Minibank.Core.Domains.Users.Validators
             RuleFor(x => x.Login).Must(it => !it.Contains(" ")).WithMessage("Login must not have some spaces!");
             RuleFor(x => x.Login.Length).LessThanOrEqualTo(20)
                 .WithMessage("Login's length must not be more than 20 symbols!");
-            RuleFor(x => x.Login).Matches(@"^git [a-zA-Z0-9]+$").WithMessage("Login is not in correct format!");
+            RuleFor(x => x.Login).Matches(@"^[a-zA-Z0-9]+$").WithMessage("Login is not in correct format!");
             RuleFor(x => x).Must((x) => !userRepository.ContainsLogin(x.Login).Result)
                 .WithMessage("This login is already used!");
             RuleFor(x => x.Email).NotEmpty().WithMessage("Email must not be empty!");
